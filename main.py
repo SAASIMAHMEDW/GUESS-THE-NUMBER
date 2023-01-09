2731327de1845d467675e44ee404960f16040aea
AASIMAHMED SIDDIQUI W <93717619+SAASIMAHMEDW@users.noreply.github.com>
2023-01-09 16:24:22 +0000

    Added some basic functionality ( Total_guesses, Total_guesses_left, total_guesses_took (

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ AUTHOR : AHMED-ORACLE
 DATE : 09 | 01 |2023
 PROGRAM : A SIMPLE GAME 
 """
-
+#Accessing the random value
 from random_number import r_num
 #import random_number 
 import colorama
@@ -21,34 +21,54 @@ print("•YOU HAVE "+colorama.Fore.CYAN+"LIMITED CHANCES"+colorama.Fore.BLACK)
 print("•TO GUESS THE "+colorama.Fore.CYAN+"CORRECT NUMBER"+colorama.Fore.BLACK)
 print("•TOTAL CHANCES"+colorama.Fore.CYAN+" 7\n"+colorama.Fore.BLACK)
 
-print("TO BEGIN THE GAME PLEASE ENTER \n"+colorama.Fore.CYAN+"(EN) FOR ENTER"+colorama.Fore.BLACK+" // "+colorama.Fore.CYAN+"(EX) FOR EXIT\n"+colorama.Fore.RESET)
+print("TO START THE GAME PLEASE ENTER \n"+colorama.Fore.CYAN+"(EN) FOR ENTER"+colorama.Fore.BLACK+" // "+colorama.Fore.CYAN+"(EX) FOR EXIT\n"+colorama.Fore.RESET)
 start_game = input("=>").upper()
 #print("\n")
-if start_game == "EN":
+#global guessesLeft
 
+if start_game == "EN":
+	totalGuesses = 7
+	gameOver = 0
+	numberOfGuessesTook = 0
+	print("\nTOTAL GUESSES ",totalGuesses)
 	while True:
+		totalGuesses = totalGuesses - 1
+		guessesLeft = totalGuesses
+		numberOfGuessesTook+=1
 		print("\nENTER YOUR GUESSED NUMBER")
 		user_guessed_num = int(input("=>"))
-		#print ("\n")
-		if user_guessed_num == r_num:
+		if guessesLeft == gameOver:
+			print("GMAE OVER")
+			print("YOU ARE OUT OF CHANCES")
+			break
+		elif user_guessed_num == r_num:
 			print("CONGRATULATIONS\nYOU HAVE GUESSED CORRECT")
+			print(f"YOU TOOK {numberOfGuessesTook} CHANCE(S)")
 			break
 		elif user_guessed_num > r_num:
+			
 			print("TOO HIGH")
+			print("GUESSES LEFT ",guessesLeft)
 		elif user_guessed_num < r_num:
+			#guessesLeft = totalGuesses -1
 			print("TOO LOW")
+			print("GUESSES LEFT ",guessesLeft)
+		# elif totalGuesses == gameOver:
+		# 	print("GAME OVER ")
 		#else:
 			#print("I AM ELSE")
 			#break
+	
 elif start_game == "EX":
-	print("YOU HAVE ENTERED EXIT")
+	print("\nYOU HAVE ENTERED EXIT")
 else:
-	print("YOU HAVE ENTERED WRONG PROMT")
+	print("\nYOU HAVE ENTERED WRONG PROMT")
 
 
 # global totalGuesses 
 # totalGuesses = 7
-# guessesLeft = totalGuesses - 1
+# totalGuesses = totalGuesses - 1
+# guessesLeft = totalGuesses
 # numberOfGuessTook = None
 # gameOver = 0
 # if totalGuesses == gameOver:
